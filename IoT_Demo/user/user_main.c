aed8bc687bba55a664e72a536f34bc8bb1abaecd
RogerHuang-rongge <30836982+RogerHuang-rongge@users.noreply.github.com>
2017-10-28 05:54:46 +0800

    source insight project build
    Signed-off-by: RogerHuang-rongge <30836982+RogerHuang-rongge@users.noreply.github.com>

--- IoT_Demo/user/user_main.c
+++ IoT_Demo/user/user_main.c
@@ -120,9 +120,9 @@ user_init(void)
     /*3 Channel mode: curl -X POST -H "Content-Type:application/json" -d "{\"period\":1000,\"rgb\":{\"red\":16000,\"green\":16000,\"blue\":16000}}" http://192.168.4.1/config?command=light      */
     /*5 Channel mode: curl -X POST -H "Content-Type:application/json" -d "{\"period\":1000,\"rgb\":{\"red\":16000,\"green\":16000,\"blue\":16000,\"cwhite\":3000,\"wwhite\",3000}}" http://192.168.4.1/config?command=light      */
 #ifdef SERVER_SSL_ENABLE
-    user_webserver_init(SERVER_SSL_PORT); //roger1
+    user_webserver_init(SERVER_SSL_PORT); //roger
 #else
-   user_webserver_init(SERVER_PORT);  //roger1
+   user_webserver_init(SERVER_PORT);  //roge
 #endif
 }
 
